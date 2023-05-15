d934c2c0da0fad51b84234b39bb9320ad6d71f13
William Ryan <billyryan543@gmail.com>
2023-05-15 00:02:35 -0400

    Improve prepare_data and generate_themes

--- R/generate_themes.R
+++ R/generate_themes.R
@@ -14,81 +14,80 @@
 #' @export
 generate_themes <-
   function(PAVER_result, hclust_method = "ward.D2", ...) {
-    #Cluster pathway embeddings using cossine similarity
-    D_sim = cosine_dissimilarity(PAVER_result$embedding_mat)
+    #Cluster pathway embeddings using cossine similarity and hierarchical clustering
+    D_sim = cosine_dissimilarity(
+      PAVER_result$embedding_mat %>%
+        tibble::column_to_rownames("UniqueID") %>%
+        as.matrix()
+    )
 
     clust = stats::hclust(D_sim, method = hclust_method)
 
-    #Dynamic tree to generate clusters
+    #Dynamic tree cut to generate clusters
     clustering = dynamicTreeCut::cutreeDynamic(clust, distM = as.matrix(D_sim), ...) %>%
       purrr::set_names(clust$labels) %>%
-      tibble::enframe() %>%
-      dplyr::rename(UniqueID = 1, Cluster = 2) %>%
-      dplyr::mutate(Cluster = forcats::as_factor(.data$Cluster)) %>%
-      dplyr::inner_join(PAVER_result$embedding_mat %>%
-                          tibble::as_tibble(rownames=NA) %>%
-                          tibble::rownames_to_column("UniqueID"), by = "UniqueID")
+      tibble::enframe(name = "UniqueID", value = "Cluster") %>%
+      dplyr::mutate(Cluster = as.factor(.data$Cluster)) %>%
+      dplyr::inner_join(PAVER_result$embedding_mat)
 
+    #Average the embeddings within each cluster
     avg_cluster_embeddings = clustering %>%
       dplyr::group_by(.data$Cluster) %>%
-      dplyr::summarise(dplyr::across(-.data$UniqueID, mean))
+      dplyr::summarise(dplyr::across(!.data$UniqueID, mean))
 
-    #Iterate through each cluster to find the embedding closest to the cluster average
-    nearestpathways = list()
-    for (i in 1:nlevels(clustering$Cluster)) {
-      top = ""
-      sim = 2 #Worst case cosine distance
-
-      #Grab all embeddings within the cluster
+    #Identify pathways most similar to the average of each cluster i.e., it's "theme"
+    nearestpathways = purrr::map_chr(1:nlevels(clustering$Cluster), function(i) {
+      # Grab all embeddings within the cluster
       cluster_embeddings = clustering %>%
         dplyr::filter(.data$Cluster == i) %>%
         dplyr::select(-.data$Cluster) %>%
         tibble::column_to_rownames("UniqueID")
 
-      #Grab the average embedding of the cluster
+      # Grab the average embedding of the cluster
       avg_cluster_embedding = avg_cluster_embeddings %>%
         dplyr::filter(.data$Cluster == i) %>%
         dplyr::select(-.data$Cluster) %>%
         as.numeric()
 
-      #Iterate through every embedding within the cluster to see which is closest to the average cluster embedding
-      for (pathway in 1:nrow(cluster_embeddings)) {
-        embedding = cluster_embeddings[pathway,] %>% as.numeric()
-        local_sim = cosine_dissimilarity(rbind(avg_cluster_embedding, embedding)) %>% as.numeric()
-        if (local_sim <= sim) {
-          top = rownames(cluster_embeddings)[pathway]
-          sim = local_sim
-        }
-      }
-      nearestpathways <- append(nearestpathways, top)
-    }
+      # Calculate cosine dissimilarity for each embedding in the cluster
+      dissimilarities = cluster_embeddings %>%
+        dplyr::group_nest(dplyr::row_number()) %>%
+        dplyr::pull() %>%
+        purrr::map_dbl( ~ cosine_dissimilarity(as.matrix(rbind(
+          avg_cluster_embedding, .x
+        ))))
 
-    #Set the cluster names to the identified themes
-    nearestpathwaysdf <- nearestpathways %>%
-      tibble::enframe() %>%
-      dplyr::rename(Cluster = .data$name, UniqueID = .data$value) %>%
-      dplyr::mutate(UniqueID = unlist(.data$UniqueID)) %>%
+      # Return the name of the embedding with smallest dissimilarity
+      rownames(cluster_embeddings)[which.min(dissimilarities)]
+    }) %>%
+      tibble::enframe(name = "Cluster", value = "UniqueID") %>%
       dplyr::mutate(GOID = stringr::str_split_i(.data$UniqueID, "_", 1)) %>%
       dplyr::inner_join(PAVER_result$goterms_df, by = "GOID")
 
-    levels(clustering$Cluster) <- nearestpathwaysdf$Term
-    levels(avg_cluster_embeddings$Cluster) <- nearestpathwaysdf$Term
+    #Set the cluster names to the identified themes
+    levels(clustering$Cluster) = nearestpathways$Term
+    levels(avg_cluster_embeddings$Cluster) = nearestpathways$Term
 
     #Reorder the clusters by similarity
-    clust2 = cosine_dissimilarity(avg_cluster_embeddings %>%
-                                    tibble::column_to_rownames("Cluster") %>%
-                                    as.matrix()) %>%
+    clust2 = avg_cluster_embeddings %>%
+      tibble::column_to_rownames("Cluster") %>%
+      as.matrix() %>%
+      cosine_dissimilarity() %>%
       stats::hclust(method = hclust_method)
 
     clustering <- clustering %>%
       dplyr::mutate(Cluster = factor(.data$Cluster, levels = clust2$labels[clust2$order])) %>%
-      tidyr::separate(.data$UniqueID, into = c("GOID", "Group"), sep = "_", remove = F) %>%
+      tidyr::separate(
+        .data$UniqueID,
+        into = c("GOID", "Group"),
+        sep = "_",
+        remove = F
+      ) %>%
       dplyr::inner_join(PAVER_result$goterms_df, by = "GOID")
 
     avg_cluster_embeddings <- avg_cluster_embeddings %>%
       dplyr::mutate(Cluster = factor(.data$Cluster, levels = clust2$labels[clust2$order]))
 
-    #Compute MDS of average cluster embeddings
     #Compute the MDS of the average embedding matrix
     mds = avg_cluster_embeddings %>%
       tibble::column_to_rownames("Cluster") %>%
@@ -99,6 +98,7 @@ generate_themes <-
     #Generate distinct categorical colors for plotting
     colors = randomcoloR::distinctColorPalette(k = nlevels(avg_cluster_embeddings$Cluster))
 
-    c(PAVER_result, tibble::lst(clustering, avg_cluster_embeddings, mds, colors))
+    c(PAVER_result,
+      tibble::lst(clustering, avg_cluster_embeddings, mds, colors))
 
   }
--- R/prepare_data.R
+++ R/prepare_data.R
@@ -5,65 +5,51 @@
 #' @param input A data frame in wide format containing the Gene Ontology enrichment analysis results. The first column is expected to be GO IDs, e.g., GO:0000001
 #' @param embeddings A matrix containing the precomputed Gene Ontology embeddings
 #' @param ontology_index An ontology_index object prepared from the core Gene Ontology used to compute embeddings
-#' @param seed An integer setting the seed for UMAP
-#' @param unit A character describing the unit of the enrichment analysis results
 #'
-#' @return A PAVER_result list with the prepared data, embedding matrix, GO Term ID to definition mappings, and computed UMAP
+#' @return A PAVER_result list with the prepared data ready for theme generation
 #'
 #' @examples
 #' TRUE
 #'
 #' @export
-prepare_data <- function(input, embeddings, ontology_index, unit = "NES", seed = 123) {
-
-  #Ensure first column is named GOID and NAs are 0.
-  input = input %>%
-    dplyr::rename(GOID = 1) %>%
-    dplyr::mutate(dplyr::across(
-      .cols = c(!.data$GOID),
-      .fns = ~ tidyr::replace_na(.x, 0)))
-
-  #Transform the named embedding matrix to a keyed wide format tibble
-  embeddings = embeddings %>%
-    tibble::as_tibble(rownames = NA, .name_repair = "unique") %>%
-    tibble::rownames_to_column("GOID")
+prepare_data <- function(input, embeddings, ontology_index) {
 
   #Transform wide format input to long form
   #Determine direction of enrichment from input value
-  #Generates a unique ID for each pathway based on GOID and Group
+  #Generates a unique ID for each pathway based on its GOID and Group
   prepared_data = input %>%
-    tidyr::pivot_longer(cols = -1, names_to = "Group") %>%
-    dplyr::group_by(.data$Group) %>%
+    dplyr::rename(GOID = 1) %>%
+    dplyr::filter(.data$GOID %in% rownames(embeddings)) %>% #Input GOIDs must be present in the embedding matrix.
+    dplyr::mutate(dplyr::across(!.data$GOID, ~ tidyr::replace_na(.x, 0))) %>%
+    tidyr::pivot_longer(cols = c(!"GOID"), names_to = "Group") %>%
     dplyr::filter(.data$value != 0) %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate(Direction = ifelse(.data$value >= 0, "Up", NA_character_)) %>%
-    dplyr::mutate(Direction = ifelse(.data$value < 0, "Down", .data$Direction)) %>%
-    dplyr::mutate(Direction = as.factor(.data$Direction)) %>%
-    dplyr::mutate(UniqueID = paste0(.data$GOID, "_", .data$Group)) %>%
+    dplyr::mutate(
+      Direction = factor(sign(.data$value), levels = c(-1, 1), labels = c("-", "+")),
+      UniqueID = paste0(.data$GOID, "_", .data$Group)) %>%
     dplyr::distinct(.data$UniqueID, .keep_all = T)
 
-  #Generate a named embedding matrix from the prepared data
-  embedding_mat = prepared_data %>%
-    dplyr::inner_join(embeddings, by = "GOID") %>%
-    dplyr::select(-c("value", "Direction", "Group", "GOID")) %>%
-    tibble::column_to_rownames("UniqueID") %>%
-    as.matrix()
+  #Generate an embedding table keyed by unique pathway IDs
+  embedding_mat = embeddings[prepared_data$GOID,] %>%
+    magrittr::set_rownames(prepared_data$UniqueID) %>%
+    tibble::as_tibble(rownames = "UniqueID")
 
   #Compute the UMAP of the embedding matrix
   custom.config = umap::umap.defaults
-  custom.config$random_state = seed
+  custom.config$random_state = 123
   custom.config$metric = "cosine"
+  custom.config$n_neighbors = 100
 
-  umap = umap::umap(embedding_mat, custom.config)
+  umap = umap::umap(embedding_mat %>% tibble::column_to_rownames("UniqueID"), custom.config)
 
-  #Generate a GO term ID to term definition mapping table
+  #Generate a GO ID to GO Term mapping table
   goterms_df = ontology_index %>%
     purrr::pluck("name") %>%
     tibble::enframe() %>%
-    dplyr::rename(GOID = 1, Term = 2)
+    dplyr::rename(GOID = 1, Term = 2) %>%
+    dplyr::filter(.data$GOID %in% prepared_data$GOID)
 
   #Return list of data ready for theme generation
-  PAVER_result = tibble::lst(prepared_data, embedding_mat, goterms_df, umap, unit)
+  PAVER_result = tibble::lst(prepared_data, embedding_mat, umap, goterms_df)
 
   PAVER_result
 
