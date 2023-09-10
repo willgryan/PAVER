6545dd6d7cb870517c454a0d2617bde8326b928f
William Ryan <billyryan543@gmail.com>
2023-09-10 01:45:30 -0400

    Start adding tests
    Try to fix rworkflows

---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,10 @@
+test_that("Cosine dissimilarity matrix is correctly computed", {
+
+  # Test with simple matrix
+  mat <- matrix(c(.5, -.5, -.5, .5), ncol=2)
+  expect_equal(as.numeric(cosine_dissimilarity(mat)), 2)
+
+  # Test with square root
+  expect_equal(as.numeric(cosine_dissimilarity(mat, root = TRUE)), sqrt(2))
+
+})
