585448d9ceecaab016801851439122ac6b0d7d55
William Ryan <billyryan543@gmail.com>
2024-05-14 11:56:58 -0400

    Add KEGG example data
    Update readme with links.

--- R/data.R
+++ R/data.R
@@ -13,3 +13,17 @@
 #'
 #' @source <https://doi.org/10.1038/s41380-021-01205-y>
 "gsea_example"
+
+#' An example of KEGG results
+#'
+#' A wide data frame with enrichment scores (log10 FDR) from
+#' MetaboAnalyst of one comparison.
+#'
+#' @format A data frame with 183 rows and 2 columns:
+#' \describe{
+#' \item{MAP}{KEGG Pathway IDS}
+#' \item{MA}{Enrichment Scores of KEGG pathways}
+#' }
+#'
+#' @source <https://doi.org/10.1101/2023.10.13.562226>
+"kegg_example"
